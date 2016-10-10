fd21166c52f4172484248fd92e4a792179092a35
attiliocaravelli <attiliocaravelli@gmail.com>
2016-10-10 12:00:08 +0200

    English comments refactor

--- src/main/java/com/painter/validators/CommandValidator.java
+++ src/main/java/com/painter/validators/CommandValidator.java
@@ -17,31 +17,36 @@ import com.painter.models.Rule;
 import com.painter.utilities.NullCheckUtilities;
 
 public class CommandValidator implements Validator{
-	
+
 	/**
 	 * Rule factory
+	 * 
 	 */
 	private final RuleFactory rf = RuleFactory.init();
 
 	public CommandValidator() {
 	}
-	
+
 	@Override
 	public boolean isValid(String input) {
-		if (NullCheckUtilities.isNull(input)) return false;
-		Rule potentialCommand = getRule(RuleFactory.COMMAND);
-		Matcher validator = getValidator(input, potentialCommand.toString());
-		if (!validator.find()) return false;
-		// checking is a supported command by First Letter
-		String cmd = input.substring(0, 1);
-		// matching of the command parameters
-		Rule commandRule = getRule(cmd);
-		if (NullCheckUtilities.isNull(commandRule)) return false;
-		// input validation
-		validator = getValidator(input, commandRule.toString());
-		return validator.find();
+		try {
+			if (NullCheckUtilities.isNull(input)) return false;
+			Rule potentialCommandRule = getRule(RuleFactory.COMMAND);
+			if (NullCheckUtilities.isNull(potentialCommandRule)) throw new RuleNotSupportedException();
+			// input string match the rule?
+			if (!getValidator(input, potentialCommandRule.toString()).find()) return false;
+			// Check the first letter of the input string 
+			// and in case the command is found -> get command + parameters rule
+			Rule commandRule = getRule(input.substring(0, 1));
+			// has not been defined?
+			if (NullCheckUtilities.isNull(commandRule)) throw new RuleNotSupportedException();
+			// return the match with the command line typed
+			return getValidator(input, commandRule.toString()).find();
+		} catch (RuleNotSupportedException e) {
+			return false;
+		}
 	}
-	
+
 	private Matcher getValidator(String input, String predicate) {
 		Pattern parsingPattern = Pattern.compile(predicate);     
 		return parsingPattern.matcher(input); 
